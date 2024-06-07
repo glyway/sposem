f9592156abca92eb49911f430bf10521cc358983
glyway <55627397+glyway@users.noreply.github.com>
2024-06-07 08:41:19 +0400

    Tooltips

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { EdgeTableComponent } from './edge-table/edge-table.component';
 import { MatSelectModule } from '@angular/material/select';
 import { VertexTableComponent } from './vertex-table/vertex-table.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { VertexTableComponent } from './vertex-table/vertex-table.component';
     MatTableModule,
     MatCardModule,
     MatIconModule,
-    MatSelectModule
+    MatSelectModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
